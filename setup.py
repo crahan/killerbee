8d40042f58879481221bbdae93d593616dea7757
CraHan <crahan@n00.be>
2025-05-02 00:05:11 +0200

    Fix chicken and egg problem. pyusb and scapy are installed as dependencies.

--- setup.py
+++ setup.py
@@ -9,55 +9,6 @@ except ImportError:
     print("No setuptools found, attempting to use distutils instead.")
     from distutils.core import setup, Extension
 
-err = []
-warn = []
-apt_get_pkgs = []
-pip_pkgs = []
-
-#Check pyUSB 1.x dependency present.
-#Support for 0.x is deprecated and being removed.
-try:
-    import usb
-except ImportError:
-    err.append("usb")
-    apt_get_pkgs.append("python-usb")
-
-try:
-    import usb.core
-except ImportError:
-    err.append("You are using pyUSB 0.x. Upgrade to pyUSB 1.x.")
-
-# TODO: Ideally we would detect missing python-dev and libgcrypt-dev to give better errors.
-
-# Dot15d4 is a dep of some of the newer tools
-try:
-    from scapy.all import Dot15d4
-except ImportError:
-    warn.append("Scapy (with dot15d4 layer)")
-    pip_pkgs.append("scapy")
-
-if len(err) > 0:
-    print("""
-Library requirements not met.  Install the following libraries, then re-run the setup script.
-
-{}\n""".format('\n'.join(err)), file=sys.stderr)
-
-if len(warn) > 0:
-    print("""
-Library recommendations not met. For full support, install the following libraries, then re-run the setup script.
-
-{}\n""".format('\n'.join(warn)), file=sys.stderr)
-
-if len(apt_get_pkgs) > 0 or len(pip_pkgs) > 0:
-    print("The following commands should install these dependencies on Ubuntu, and can be adapted for other OSs:", file=sys.stderr)
-    if len(apt_get_pkgs) > 0:
-        print("\tsudo apt-get install -y {}".format(' '.join(apt_get_pkgs)), file=sys.stderr)
-    if len(pip_pkgs) > 0:
-        print("\tpip3 install {}".format(' '.join(pip_pkgs)), file=sys.stderr)
-
-if len(err) > 0:
-    sys.exit(1)
-
 zigbee_crypt = Extension('zigbee_crypt',
                          sources = ['zigbee_crypt/zigbee_crypt.c'],
                          libraries = ['gcrypt'],
